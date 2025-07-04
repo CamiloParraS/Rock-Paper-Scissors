7a9770ef7c299a424f4c5d2b07a1904e12b41f78
CamiloParraS <juanparracsfn@gmail.com>
2025-07-04 10:48:14 -0500

    Implement computer choice logic for Rock-Paper-Scissors game

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+// The Logic Behind the computer choice
+
+function getComputerChoice() {
+  let computerChoice;
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  switch (randomNumber) {
+    case 1:
+      computerChoice = "Rock";
+      break;
+    case 2:
+      computerChoice = "Paper";
+      break;
+    case 3:
+      computerChoice = "Scissors";
+      break;
+  }
+  return computerChoice;
+}
+
+getComputerChoice();
