06a5a9fafe7d8ecdd7d1aa8147f4197fa295b9a3
CamiloParraS <juanparracsfn@gmail.com>
2025-07-04 11:07:44 -0500

    Add human choice logic to Rock-Paper-Scissors game

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// Variables Scores
+let computerScore = 0;
+let humanScore = 0;
+
 // The Logic Behind the computer choice
 
 function getComputerChoice() {
@@ -18,3 +22,25 @@ function getComputerChoice() {
 }
 
 getComputerChoice();
+
+// Logic behind the Humna choice
+
+function getHumanChoice() {
+  let humanChoice;
+  let sign = prompt("Choose: 1 for Rock, 2 for Paper, 3 for Scissors");
+
+  switch (sign) {
+    case "1":
+      humanChoice = "Rock";
+      break;
+    case "2":
+      humanChoice = "Paper";
+      break;
+    case "3":
+      humanChoice = "Scissiors";
+      break;
+  }
+  console.log(`Human choice: ${humanChoice}`);
+  return humanChoice;
+}
+getHumanChoice();
